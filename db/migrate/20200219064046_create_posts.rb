0d9ea1280d41b713f5272e8c6507fac2eec9eb66
jumpei arai <jumpei0924@gmail.com>
2020-02-21 19:56:16 +0900

    投稿model作成

---
+++ db/migrate/20200219064046_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :name, null: false
+      t.string :text, null: false
+      t.string :address, null: false
+      t.string :image,  null: false
+      t.float  :latitude
+      t.float  :longitude
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_080911) do
+ActiveRecord::Schema.define(version: 2020_02_19_064046) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "text", null: false
+    t.string "address", null: false
+    t.string "image", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
