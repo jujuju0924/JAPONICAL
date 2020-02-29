00f62949b85076241139fc1117df11b1213e72b5
jumpei arai <jumpei0924@gmail.com>
2020-02-29 16:07:50 +0900

    ログインページ

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :image, presence: true
-  belongs_to :user
+  has_one :user
 
 end
