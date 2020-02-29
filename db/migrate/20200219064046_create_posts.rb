28e3d4133255f7ba067833257c93ca16ab437d88
jumpei arai <jumpei0924@gmail.com>
2020-02-29 17:05:48 +0900

    Update 20200219064046_create_posts.rb

--- db/migrate/20200219064046_create_posts.rb
+++ db/migrate/20200219064046_create_posts.rb
@@ -8,7 +8,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.float  :latitude
       t.float  :longitude
       t.timestamps
-      t.integer :user_id
+      t.references :user, foreign_key: true
     end
   end
 end
