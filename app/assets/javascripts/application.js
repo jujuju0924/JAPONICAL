0f269d3c293f0fdfde7c4000db55bdc7144adc68
jumpei arai <jumpei0924@gmail.com>
2020-03-23 18:25:53 +0900

    非同期

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
 //= require underscore
 //= require gmaps/google
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/posts/#{comment.post.id}"
+    @comment = Comment.create(comment_params)
+    if @comment.save
+    respond_to do |format|
+      format.html { redirect_to "/posts/#{comment.post.id}", notice: "メッセージを送信しました" }
+      format.json
+    end
+  else
+    @coment = @user.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
   end
 
   private
